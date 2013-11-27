904a1a82c12799240700af3b67e6c4964765bf18
E-Max <max.zab87@gmail.com>
2013-11-27 17:56:03 +0200

    Delete unnecessary step in intialization

--- lib/translatable.rb
+++ lib/translatable.rb
@@ -6,11 +6,9 @@ require "translatable/base"
 if defined?(Rails)
   require 'translatable/engine'
 
-  if defined?(ActiveRecord)
-    ActiveSupport.on_load(:active_record) do
-      require 'translatable/orm/active_record'
-      ActiveRecord::Base.extend Translatable::ActiveRecord 
-    end
+  ActiveSupport.on_load(:active_record) do
+    require 'translatable/orm/active_record'
+    ActiveRecord::Base.extend Translatable::ActiveRecord 
   end
 else
   if defined?(ActiveRecord)
